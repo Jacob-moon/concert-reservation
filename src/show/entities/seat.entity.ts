0978ca803314e739be19217e84a5519848c2cfd8
Jacob-moon <53264081+Jacob-moon@users.noreply.github.com>
2024-10-24 21:09:29 +0900

    [add] 시트엔티티 추가

---
+++ src/show/entities/seat.entity.ts
@@ -0,0 +1,24 @@
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne,OneToMany } from 'typeorm';
+import { Box } from './box.entity';
+import { ScheduledSeat } from './scheduled-seat.entity';
+
+@Entity()
+export class Seat {
+  @PrimaryGeneratedColumn()
+  seatId: number;
+
+  @Column({ length: 50 })
+  zone: string; // 좌석의 구역
+
+  @Column({ length: 20 })
+  seatNumber: string; // 좌석 번호 (예: A1, B2)
+
+  @Column({ type: 'int' })
+  price: number; // 좌석 가격
+
+  @ManyToOne(() => Box, (box) => box.seats)
+  box: Box; // 좌석이 속한 구역
+
+  @OneToMany(() => ScheduledSeat, (scheduledSeat) => scheduledSeat.seat)
+  scheduledSeats: ScheduledSeat[]; // 좌석의 스케줄링 정보 (예약 여부 등)
+}
