82bbe0fa9ebcf33cf67d25d66901e8beffe86b42
Jacob-moon <53264081+Jacob-moon@users.noreply.github.com>
2024-10-24 19:49:03 +0900

    [fix] 엔티티 파일 구조 전체 변경
    기능에 따라 파일구조를 변경하였습니다.
    
    기존에는 테이블에따라 나누었는데 그렇게 큰 프로젝트가 아닐것같아서 기능별로 유사한 것들끼리 나누었습니다.

--- src/scheduledseat/dto/update-scheduledseat.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateScheduledseatDto } from './create-scheduledseat.dto';
-
-export class UpdateScheduledseatDto extends PartialType(CreateScheduledseatDto) {}
--- src/scheduledseat/scheduledseat.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ScheduledseatService } from './scheduledseat.service';
-import { ScheduledseatController } from './scheduledseat.controller';
-
-@Module({
-  controllers: [ScheduledseatController],
-  providers: [ScheduledseatService],
-})
-export class ScheduledseatModule {}
