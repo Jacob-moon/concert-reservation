802767c67bc374ff199db816d1c7e02894225014
Jacob-moon <53264081+Jacob-moon@users.noreply.github.com>
2024-10-24 20:20:44 +0900

    [fix] 유저 모듈 경로 수정

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,8 +3,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { User } from './entities/user.entity';
-import { Point } from '../point/entities/point.entity';
-import { Order } from '../order/entities/order.entity';
+import { Point } from './entities/point.entity';
+import { Order } from './entities/order.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User, Point, Order])],
