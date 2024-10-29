2895377de2c8a890b32538eea9fb6c0cfcb8c01a
Jacob-moon <53264081+Jacob-moon@users.noreply.github.com>
2024-10-29 14:16:26 +0900

    [fix] 파일구조 변경에 따른 모듈 삭제

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModuleValidationSchema } from './configs/env-validation.config';
 import { TypeOrmModuleOptions } from './configs/database.config';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { ShowModule } from './show/show.module';
-import { BookModule } from './book/book.module';
-import { OrderModule } from './order/order.module';
-import { PointModule } from './point/point.module';
-import { TicketModule } from './ticket/ticket.module';
-import { ScheduleModule } from './schedule/schedule.module';
-import { SeatModule } from './seat/seat.module';
-import { ScheduledseatModule } from './scheduledseat/scheduledseat.module';
-import { BoxModule } from './box/box.module';
-import { TheaterModule } from './theater/theater.module';
 import { PaymentModule } from './payment/payment.module';
 
 @Module({
@@ -31,15 +22,6 @@ TypeOrmModule.forRootAsync(
 AuthModule,
 UserModule,
 ShowModule,
-BookModule,
-OrderModule,
-PointModule,
-TicketModule,
-ScheduleModule,
-SeatModule,
-ScheduledseatModule,
-BoxModule,
-TheaterModule,
 PaymentModule,
 ],
   controllers: [AppController],
