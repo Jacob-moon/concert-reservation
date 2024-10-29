6c4cc47650a0da11092d5c5177e8ccec5d6ffe32
Jacob-moon <53264081+Jacob-moon@users.noreply.github.com>
2024-10-29 23:08:31 +0900

    [fix] jwt.strategy 파일 올바른 위치로 이동

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -2,9 +2,10 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy, ExtractJwt } from 'passport-jwt';
 import { ConfigService } from '@nestjs/config';
-import {UserService} from '../user/user.service';
-import { JwtPayload } from './jwt-payload.interface';
-import { User } from '../user/entities/user.entity';
+import { UserService } from 'src/user/user.service';
+import { JwtPayload } from '../jwt-payload.interface';
+import { User } from 'src/user/entities/user.entity';
+
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
