55f3889f3cfb5b8cd8c4bc91fdf578d1166daf63
Jacob-moon <53264081+Jacob-moon@users.noreply.github.com>
2024-10-29 15:04:54 +0900

    [add] 회원가입 로직 작성중

--- src/auth/dto/sign-up.dto.ts
+++ src/auth/dto/sign-up.dto.ts
@@ -1,11 +1,11 @@
-// import { PickType } from '@nestjs/mapped-types';
-// import { User } from '../../user/entities/user.entity';
+import { PickType } from '@nestjs/mapped-types';
+import { User } from '../../user/entities/user.entity';
 
-// export class SignUpDto extends PickType(User,[
-//     'email',
-//     'password',
-//     'nickname',
-// ]){
-//     passwordConfirm: string;
-// }
+export class SignUpDto extends PickType(User,[
+    'email',
+    'password',
+    'nickname',
+]){
+    passwordConfirm: string;
+}
 
